866efe9fedeca5ba6b2d7d13e735ff9bc4fe9320
satoshi yamada <satoshi.yamada@sint.co.jp>
2022-08-13 12:49:12 +0900

    feat: add sample server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const Fastify = require("fastify");
+const mercurius = require("mercurius");
+
+const fastify = Fastify();
+
+const schema = `
+  type Query {
+    add(x: Int, y: Int): Int
+  }
+`;
+
+const resolvers = {
+  Query: {
+    add: async (_, { x, y }) => x + y,
+  },
+};
+
+fastify.register(mercurius, {
+  schema,
+  resolvers,
+});
+
+fastify.get("/", async function (_req, reply) {
+  const query = "{ add(x: 2, y: 2) }";
+  return reply.graphql(query);
+});
+
+fastify.listen({ port: 3000 }, (err) => {
+  if (err) throw err;
+  console.log(`server listening on ${fastify.server.address().port}`);
+});
